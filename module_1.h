84604e508217a05e542b0de7ded9a483553f2731
Eric Howard <domhoward14@gmail.com>
2015-05-29 15:33:44 -0400

    updated version

---
+++ module_1.h
@@ -0,0 +1,17 @@
+#ifndef MODULE_1_H_INCLUDED
+#define MODULE_1_H_INCLUDED
+
+/*add prototypes here*/
+void load();
+void takingInput();
+void fetch ();
+void getInstruction();
+int isDone ();
+void execute();
+void initArray();
+int getBase(int level, int base);//tested
+void testFunction();//testing purposes
+void lit (int pushValue);
+
+
+#endif /* MODULE_1_H_INCLUDED*/
